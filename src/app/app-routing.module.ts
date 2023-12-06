aa4a345567c242fb4bf197c98eb1257413f905d4
kalimu7 <mahjourkarim@gmail.com>
2023-12-06 09:15:14 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminCenterComponent } from './Components/admin-center/admin-center.component';
+import { HomeComponent } from './Components/home/home.component';
+import { PromotionComponent } from './Components/promotion/promotion.component';
+const routes: Routes = [
+  {path:'AdminCenter',component: AdminCenterComponent },
+  {path:'home',component: HomeComponent},
+  {path:'promotion',component: PromotionComponent }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
